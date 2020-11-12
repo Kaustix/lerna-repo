1e11be92f436ff813a53ab63932e58925854f36f
Kiernan Lamarche <kiernan.lamarche@gmail.com>
2020-11-12 16:53:47 -0700

    changed things

--- packages/flux/index.js
+++ packages/flux/index.js
@@ -3,3 +3,5 @@ export { default as createAction } from "./lib/actions/create-action";
 export { default as createTypes } from "./lib/actions/create-types";
 export { default as thunk } from "./lib/actions/thunk-request";
 export { default as createReducer } from "./lib/reducers/create-reducer";
+
+console.log("hello");
--- packages/utils/index.js
+++ packages/utils/index.js
@@ -9,3 +9,5 @@ export const isArrayEmpty = (array) => !Array.isArray(array) || !array.length;
 
 export const dateFormat = "dd/MM/yyyy";
 export const toDateString = (value) => format(value, dateFormat);
+
+console.log("asdf");
